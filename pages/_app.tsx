1f907355d572c3614a2b33bb9f85c666fa2722c6
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-07-05 20:43:10 -0500

    Alter App Icon & Title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../public/css/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) 
+{
+  return (
+    <>
+      <Head>
+        <title>{"OU Game Developer's Association"}</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
