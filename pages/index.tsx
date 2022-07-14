f6bc3cb8bb3b4ed640b5144ff9719c052edc3823
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-07-13 21:04:55 -0500

    Home Page Header Text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,32 @@
 import type { NextPage } from 'next';
+
+import Image from 'next/image';
 import Header from '../public/components/Header';
 
 import HeaderImage from '../public/images/placeholder.jpg';
+import Logo from '../public/images/gdaLogo.png';
+
+const DiscordLink: string = 'https://discord.gg/z4fZ8UXa';
+const NewsletterLink: string = '';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Header src={HeaderImage} fadeColor='#0F1113' overlay={<>Hello World!</>}/>
+      <Header src={HeaderImage} fadeColor={'#0F1113'} overlay={
+        <>
+          <div>
+            <h2>The University of Oklahoma&apos;s</h2>
+            <h1>Game Developer&apos;s Association</h1>
+            <h2>─────────────────────</h2>
+            <p>Join us on our&nbsp;
+              <a href={DiscordLink}>Discord Server</a>
+            </p>
+            <p>Or sign up for our&nbsp;
+              <a href={NewsletterLink}>Newsletter</a>
+            </p>
+          </div>
+        </>
+      }/>
     </>
   );
 }
