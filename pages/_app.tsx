1cf52786bbf5c40ef000228824a333688d85b919
Skyler Riggle <Skyler.D.Riggle-1@ou.edu>
2022-07-13 19:20:25 -0500

    Header Fade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../public/css/globals.css';
+import '../public/css/gda-styles.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
--- public/components/Header.tsx
+++ public/components/Header.tsx
@@ -13,7 +13,7 @@ const Header = (props: HeaderProps) => {
     const ratio: number = props.src.width / props.src.height;
 
     return (
-        <>
+        <div className="layered-container" style={{height:'75vh'}}>
             <Image 
                 src={props.src} 
                 alt={props.alt}
@@ -21,10 +21,17 @@ const Header = (props: HeaderProps) => {
                 width={100 + 'vw'}
                 height={(100 / ratio) + 'vw'}
             />
-            <div>
+
+            <div className="overlay">
                 {props.overlay}
+
+                <div className="overlay" style={{
+                    width:'100%', 
+                    height:'100%', 
+                    background:'linear-gradient(transparent, ' + props.fadeColor + ' 90%)'
+                }}/>
             </div>
-        </>
+        </div>
     );
 }
 
